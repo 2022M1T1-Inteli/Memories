e9022e582ad5d32cb442bcc8a267406af11f3f1f
Rafael-Cabral <99192195+Rafael-Cabral@users.noreply.github.com>
2022-03-29 15:47:35 -0300

    Resolvi BUGS fase Racial.

--- src/Memories/Scenes/Level/Racial/Branco Venceu/Branco Venceu.gd
+++ src/Memories/Scenes/Level/Racial/Branco Venceu/Branco Venceu.gd
@@ -1,7 +1,10 @@
 extends Node2D
 var cont = 0
 
-func _physics_process(delta):
-	cont += 1
-	if cont == 300:
+func _ready():
+	$Timer.start()
+	
+#temporizador do tempo da cena perdeu
+func _process(delta):
+	if $Timer.time_left == 0:
 		get_tree().change_scene("res://Scenes/Level/Racial/World/Mundo.tscn")
