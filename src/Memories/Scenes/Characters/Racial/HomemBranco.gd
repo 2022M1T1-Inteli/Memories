398e5ea129669a926af33751af4e99ab99cb009c
Luiz Felipe <luizkama28@gmail.com>
2022-04-05 15:10:58 -0300

    Racial animation

--- src/Memories/Scenes/Characters/Racial/HomemBranco.gd
+++ src/Memories/Scenes/Characters/Racial/HomemBranco.gd
@@ -9,6 +9,7 @@ var FLOOR = Vector2(0, -1)
 #	iniciar o jogo somente quando o espaço for apertado
 func _physics_process(delta):
 	if Input.is_action_just_pressed("jump"):
+		$AnimatedSprite.play("run")
 		iniciou = true
 
 #	Definir a velocidade com a qual o jogador irá correr 
